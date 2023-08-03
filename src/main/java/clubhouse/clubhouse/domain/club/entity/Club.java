8bea70a215072d8cefd7081180bba2ff516b2dcd
minomistone <2013118@sookmyung.ac.kr>
2023-08-03 15:10:42 +0900

    feat: club entity에 club_intro(한 줄 소개) column 추가

--- src/main/java/clubhouse/clubhouse/domain/club/entity/Club.java
+++ src/main/java/clubhouse/clubhouse/domain/club/entity/Club.java
@@ -13,30 +13,35 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class Club {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "club_id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "club_id")
+    private Long id;
 
-	@Column(name = "club_name")
-	private String name;
+    @Column(name = "club_name")
+    private String name;
 
-	@Column(name = "club_admin_id")
-	private Long adminId;
+    @Column(name = "club_admin_id")
+    private String adminId;
 
-	@Column(name = "club_cat")
-	@Enumerated(EnumType.STRING)
-	private CategoryName categoryName;
+    @Column(name = "club_cat")
+    @Enumerated(EnumType.STRING)
+    private CategoryName categoryName;
 
-	@OneToMany(mappedBy = "club")
-	private List<MemberClub> memberClubs = new ArrayList<>();
+    @Column(name = "club_intro")
+    private String intro;
 
-	public Club(String name, CategoryName categoryName) {
-		this.name = name;
-		this.categoryName = categoryName;
-	}
+    @OneToMany(mappedBy = "club")
+    private List<MemberClub> memberClubs = new ArrayList<>();
 
-	//	public Club(Long id, String name, Long adminId, CategoryName categoryName){
+    public Club(String name, CategoryName categoryName, String adminId, String intro) {
+        this.name = name;
+        this.categoryName = categoryName;
+        this.adminId = adminId;
+        this.intro = intro;
+    }
+
+    //	public Club(Long id, String name, Long adminId, CategoryName categoryName){
 //		this.id = id;
 //		this.name = name;
 //		this.adminId = adminId;
