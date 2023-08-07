62a40908ff327eede2cd77bc83cca7d6e18ccd39
youth <boysoeng@naver.com>
2023-08-07 17:30:10 +0900

    배포용 브랜치 초기 설정

---
+++ src/main/java/clubhouse/clubhouse/domain/club/repository/ClubRepository.java
@@ -0,0 +1,9 @@
+package clubhouse.clubhouse.domain.club.repository;
+
+import clubhouse.clubhouse.domain.club.entity.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClubRepository extends JpaRepository<Club,Long> {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into club(club_name,club_admin_id,club_cat)
+            values('cotato',1,'IT');
+insert into club(club_name,club_admin_id,club_cat)
+            values('dnd',2,'IT');
+insert into club(club_name,club_admin_id,club_cat)
+            values('nexters',3,'IT');
